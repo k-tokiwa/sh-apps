c54048cb90dd8f4fbeb042ffcfa88bf7ad03cfcb
Shuma Yoshioka <s64.stdio@gmail.com>
2015-04-25 07:35:00 +0900

    Update

---
+++ volumes/wordpress-scripts/install_init.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+wp plugin install google-sitemap-generator
+wp plugin install pushpress
+wp plugin install wpmandrill
+
+wp plugin install twitter
+wp plugin install facebook
+wp plugin install slack
+
+wp plugin install broken-link-checker
+wp plugin install wp-basic-auth
+wp plugin install wordfence
+wp plugin install backwpup
+wp plugin install video-thumbnails
+
+wp theme install wp-jurist
+wp theme install hueman
+wp theme install flower
